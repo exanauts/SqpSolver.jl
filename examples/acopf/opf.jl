57950940308ac8f71979f8b1453283945cb937bb
Kibaek Kim <kibaek.k@gmail.com>
2021-10-20 11:02:39 -0500

    allow removing angle diff constraints

--- examples/acopf/opf.jl
+++ examples/acopf/opf.jl
@@ -9,15 +9,48 @@ PowerModels.silence()
 include("acwr.jl")
 include("init_opf.jl")
 
-build_acp(data_file::String) = instantiate_model(PowerModels.parse_file(data_file), ACPPowerModel, PowerModels.build_opf)
-build_acr(data_file::String) = instantiate_model(PowerModels.parse_file(data_file), ACRPowerModel, PowerModels.build_opf)
+function build_opf(pm::PowerModels.AbstractPowerModel)
+    PowerModels.variable_bus_voltage(pm)
+    PowerModels.variable_gen_power(pm)
+    PowerModels.variable_branch_power(pm)
+    PowerModels.variable_dcline_power(pm)
+
+    PowerModels.objective_min_fuel_and_flow_cost(pm)
+
+    PowerModels.constraint_model_voltage(pm)
+
+    for i in PowerModels.ids(pm, :ref_buses)
+        PowerModels.constraint_theta_ref(pm, i)
+    end
+
+    for i in PowerModels.ids(pm, :bus)
+        PowerModels.constraint_power_balance(pm, i)
+    end
+
+    for i in PowerModels.ids(pm, :branch)
+        PowerModels.constraint_ohms_yt_from(pm, i)
+        PowerModels.constraint_ohms_yt_to(pm, i)
+
+        # constraint_voltage_angle_difference(pm, i)
+
+        PowerModels.constraint_thermal_limit_from(pm, i)
+        PowerModels.constraint_thermal_limit_to(pm, i)
+    end
+
+    for i in PowerModels.ids(pm, :dcline)
+        PowerModels.constraint_dcline_power_losses(pm, i)
+    end
+end
+
+build_acp(data_file::String) = instantiate_model(PowerModels.parse_file(data_file), ACPPowerModel, build_opf)
+build_acr(data_file::String) = instantiate_model(PowerModels.parse_file(data_file), ACRPowerModel, build_opf)
 build_iv(data_file::String) = instantiate_model(PowerModels.parse_file(data_file), IVRPowerModel, PowerModels.build_opf_iv)
 build_dcp(data_file::String) = instantiate_model(PowerModels.parse_file(data_file), DCPPowerModel, PowerModels.build_opf_iv)
 
 ##
 function run_sqp_opf(data_file::String, max_iter::Int = 100)
-    pm = build_acp(data_file)
-    init_vars(pm)
+    pm = build_acr(data_file)
+    # init_vars(pm)
     # pm2 = build_acp(data_file)
     # JuMP.@objective(pm2.model, Min, 0)
     # init_vars_from_ipopt(pm, pm2)
@@ -48,7 +81,7 @@ function run_sqp_opf(data_file::String, max_iter::Int = 100)
     return pm, result
 end
 
-run_sqp_opf("../data/case30.m", 50);
+run_sqp_opf("../data/case9.m", 50);
 
 ##
 function run_ipopt!(data_file::String)
