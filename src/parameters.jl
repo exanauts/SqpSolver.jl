815fa790766d25c6eac17249cad144fbb7e9ae23
Kibaek Kim <kibaek.k@gmail.com>
2023-05-11 20:55:08 -0500

    updated default params

--- src/parameters.jl
+++ src/parameters.jl
@@ -14,12 +14,12 @@ Base.@kwdef mutable struct Parameters
     StatisticsFlag::Int = 0   # 0 supresses collection of statistics parameters
 
     # Algorithmic parameters
-    tol_direction::Float64 = 1.e-6  # tolerance for the norm of direction
-    tol_residual::Float64 = 1.e-6   # tolerance for Kuhn-Tucker residual
-    tol_infeas::Float64 = 1.e-6     # tolerance for constraint violation
-    max_iter::Int = 1000            # Defines the maximum number of iterations
+    tol_direction::Float64 = 1.e-8  # tolerance for the norm of direction
+    tol_residual::Float64 = 1.e-8   # tolerance for Kuhn-Tucker residual
+    tol_infeas::Float64 = 1.e-8     # tolerance for constraint violation
+    max_iter::Int = 3000            # Defines the maximum number of iterations
     time_limit::Float64 = Inf       # Defines the time limit for the solver. (This hasn't been implemented yet)
-    init_mu::Float64 = 1.e+4        # initial mu value
+    init_mu::Float64 = 1.e+0        # initial mu value
     max_mu::Float64 = 1.e+10        # maximum mu value allowed
     rho::Float64 = 0.8              # parameter in (0,1) used for updating merit function penalty
     eta::Float64 = 0.4              # descent step test parameter defined in (0,0.5)
