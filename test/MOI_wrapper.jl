9f799856efe886b5c5aa1e31db623861f8f7c4e2
Kibaek Kim <kibaek.k@gmail.com>
2023-05-11 21:42:05 -0500

    completed MOI test

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -14,11 +14,12 @@ const optimizer = SQP.Optimizer()
 const ipopt_optimizer = optimizer_with_attributes(
     Ipopt.Optimizer,
     "print_level" => 0,
+    "mu_strategy" => "adaptive",
     "warm_start_init_point" => "yes",
 )
 
 MOI.set(optimizer, MOI.RawOptimizerAttribute("external_optimizer"), ipopt_optimizer)
-# MOI.set(optimizer, MOI.RawOptimizerAttribute("max_iter"), 3000)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("max_iter"), 1000)
 MOI.set(optimizer, MOI.RawOptimizerAttribute("OutputFlag"), 1)
 
 function runtests()
@@ -43,6 +44,7 @@ function test_MOI_Test()
         MOI.Test.Config(
             atol = 1e-4,
             rtol = 1e-4,
+            infeasible_status = MOI.LOCALLY_INFEASIBLE,
             optimal_status = MOI.LOCALLY_SOLVED,
             exclude = Any[
                 MOI.ConstraintDual,
@@ -53,30 +55,8 @@ function test_MOI_Test()
         );
         exclude = String[
             # Tests purposefully excluded:
-            #  - NORM_LIMIT when run on macOS-M1. See #315
-            "test_linear_transform",
-            #  - Upstream: ZeroBridge does not support ConstraintDual
-            "test_conic_linear_VectorOfVariables_2",
-            #  - Excluded because this test is optional
-            "test_model_ScalarFunctionConstantNotZero",
-            #  - Excluded because Ipopt returns NORM_LIMIT instead of
-            #    DUAL_INFEASIBLE
-            "test_solve_TerminationStatus_DUAL_INFEASIBLE",
-            #  - Excluded because Ipopt returns INVALID_MODEL instead of
-            #    LOCALLY_SOLVED
-            "test_linear_VectorAffineFunction_empty_row",
-            #  - Excluded because Ipopt returns LOCALLY_INFEASIBLE instead of
-            #    INFEASIBLE
-            "INFEASIBLE",
-            "test_conic_linear_INFEASIBLE",
-            "test_conic_linear_INFEASIBLE_2",
-            "test_solve_DualStatus_INFEASIBILITY_CERTIFICATE_",
-            #  - Excluded due to upstream issue
-            "test_model_LowerBoundAlreadySet",
-            "test_model_UpperBoundAlreadySet",
-            #  - CachingOptimizer does not throw if optimizer not attached
-            "test_model_copy_to_UnsupportedAttribute",
-            "test_model_copy_to_UnsupportedConstraint",
+            #  - Convex after reformulation; but we cannot find a global optimum.
+            "test_quadratic_SecondOrderCone_basic",
         ],
     )
     return
